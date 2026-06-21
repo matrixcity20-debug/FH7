743ac696b6fed15c816f793895ac3c952a28a977
matrixcity20-debug <matrixcity20@gmail.com>
2026-06-21 15:26:59 +0300

    Add files via upload

--- server/src/app.ts
+++ server/src/app.ts
@@ -98,7 +98,7 @@ app.use("/api", router);
 if (process.env["NODE_ENV"] === "production") {
   const publicDir = path.resolve(__dirname, "../../public");
   app.use(express.static(publicDir));
-  app.get("*", (_req, res) => {
+  app.get("(.*)", (_req, res) => {
     res.sendFile(path.join(publicDir, "index.html"));
   });
 }
